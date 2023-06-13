cd7446b5cb08c13e7e03b125cef8b28d119a6999
HibatID <muhammadhibatullah@gmail.com>
2023-06-13 19:56:02 +0800

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -7,4 +7,35 @@ private String npm, nama, prodi;
 public String getDetail(){
     return npm + nama + prodi;
 }
+
+public Mahasiswa(String npm, String nama, String prodi){
+    this.npm = npm;
+    this.nama = nama;
+    this.prodi = prodi;
+}
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
 }
